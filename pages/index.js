852c1d624df0154061d729756be8c04ba08fbfba
Jala Sh <jala.shikhaliyeva@gmail.com>
2025-02-27 16:28:20 +0400

    add favicon and improve SEO with meta tags in Home page

--- pages/index.js
+++ pages/index.js
@@ -4,17 +4,30 @@ import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import ResumeSection from "@/components/ResumeSection";
 import Skills from "@/components/Skills";
-
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
-     <Header />
-     <Hero  id="hero" />
-     <Projects  id="projects"/>
-     <Skills  id="skills"  />
-     <ResumeSection  id="education"  />
-     <Contact id="contact"  />
+      <main>
+        <Head>
+          <title>Jala Shikhaliyeva/Frontend Developer</title>
+          <meta
+            name="description"
+            content="This project is my personal portfolio website. You can find my projects, skills and more information about me here. Feel free to contact me!"
+          ></meta>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
+        </Head>
+        <Header />
+        <Hero id="hero" />
+        <Projects id="projects" />
+        <Skills id="skills" />
+        <ResumeSection id="education" />
+        <Contact id="contact" />
+      </main>
     </>
   );
 }
